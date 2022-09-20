53315203452093923533b9e99003c1b2dff96a2d
bilanaltay <bilanaltay@gmail.com>
2022-09-20 22:22:15 +0300

    Project pushed to GitHub

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div>
+    <h1>Welcome to React Router!</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
